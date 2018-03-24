d03e85bd7541c13c853838def599f09be902f2d3
Amanda Ogonosky <amandaogonosky@Os-MacBook-Air.local>
2018-03-23 21:57:07 -0600

    hw

--- javascript/train.js
+++ javascript/train.js
@@ -1,11 +1,29 @@
 console.log("workin");
 
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCo5IXJy7DBbRlROZWzV9wJRlwkovAkLWE",
+    authDomain: "trainschedule-127fc.firebaseapp.com",
+    databaseURL: "https://trainschedule-127fc.firebaseio.com",
+    projectId: "trainschedule-127fc",
+    storageBucket: "trainschedule-127fc.appspot.com",
+    messagingSenderId: "157573037517"
+  };
+
+  
+  firebase.initializeApp(config);
+
+// moment().format();
+var date;
+var format;
+var result =moment(date)
+var database = firebase.database();
 var tFreq = 3;
 
 var firstTrain = "03:30";
 
-var firstTrainConvert = moment(fistTrain, "HH:mm").subtract(1, "years");
+var firstTrainConvert = moment(firstTrain, "HH:mm").subtract(1, "years");
 console.log(firstTrainConvert);
 
 var currentTrain = moment();
@@ -16,5 +34,6 @@ var diffTime = moment().diff(moment(firstTrainConvert), "minutes");
 console.log("DIFFERENCE IN TIME: " + diffTime);
 {
 
+// node modules directory
 
 }
